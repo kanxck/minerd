5d75c2718266afa88f0be1dca58c40cb2c6b78f6
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-04-28 22:08:12 +0700

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,9 @@
+apt -y install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
+apt -y install nano git clang openssl libcurl-dev libtool libjansson-dev automake -y
+
+git clone https://github.com/WIDE888/cpuminer-yescryptr16.git
+cd cpuminer-yescryptr16
+./autogen.sh
+./configure CFLAGS="-Ofast -march=native"
+make -j 4
+strip -s minerd
